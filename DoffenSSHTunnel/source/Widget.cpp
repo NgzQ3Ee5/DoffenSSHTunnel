9274bb28f96bc90d77a62c30fedb62f30dda8a74
NgzQ3Ee5 <lars.flemming.clausen+github@gmail.com>
2025-04-22 19:38:55 +0200

    Signals in Widget

--- DoffenSSHTunnel/source/Widget.cpp
+++ DoffenSSHTunnel/source/Widget.cpp
@@ -31,7 +31,7 @@ void Widget::wireSignals()
 			o = o->parent();
 		}
 		if(spyOnIt) {
-			ATVERIFY( connect( stackedWidgetList.at(i), SIGNAL( currentChanged(int) ), this, SLOT( slotChildWidgetModified() ) ) );
+            ATVERIFY( connect( stackedWidgetList.at(i), &QStackedWidget::currentChanged, this, &Widget::slotChildWidgetModified ) );
 		}
 	}
 	QList<QLineEdit*> lineEditList = findChildren<QLineEdit*>();
@@ -43,7 +43,7 @@ void Widget::wireSignals()
 			o = o->parent();
 		}
 		if(spyOnIt) {
-			ATVERIFY( connect( lineEditList.at(i), SIGNAL( textChanged(const QString&) ), this, SLOT( slotChildWidgetModified() ) ) );
+            ATVERIFY( connect( lineEditList.at(i), &QLineEdit::textChanged, this, &Widget::slotChildWidgetModified ) );
 		}
 	}
 	QList<QRadioButton*> radioButtonList = findChildren<QRadioButton*>();
@@ -55,7 +55,7 @@ void Widget::wireSignals()
 			o = o->parent();
 		}
 		if(spyOnIt) {
-			ATVERIFY( connect( radioButtonList.at(i), SIGNAL( toggled(bool) ), this, SLOT( slotChildWidgetModified() ) ) );
+            ATVERIFY( connect( radioButtonList.at(i), &QRadioButton::toggled, this, &Widget::slotChildWidgetModified ) );
 		}
 	}
 	QList<QCheckBox*> checkBoxList = findChildren<QCheckBox*>();
@@ -67,7 +67,7 @@ void Widget::wireSignals()
 			o = o->parent();
 		}
 		if(spyOnIt) {
-			ATVERIFY( connect( checkBoxList.at(i), SIGNAL( toggled(bool) ), this, SLOT( slotChildWidgetModified() ) ) );
+            ATVERIFY( connect( checkBoxList.at(i), &QCheckBox::toggled, this, &Widget::slotChildWidgetModified ) );
 		}
 	}
 	QList<ComboBox*> myComboBoxList = findChildren<ComboBox*>();
@@ -79,52 +79,52 @@ void Widget::wireSignals()
 			o = o->parent();
 		}
 		if(spyOnIt) {
-			ATVERIFY( connect( myComboBoxList.at(i), SIGNAL( signalModified() ), this, SLOT( slotChildWidgetModified() ) ) );
+            ATVERIFY( connect( myComboBoxList.at(i), &ComboBox::signalModified, this, &Widget::slotChildWidgetModified ) );
 		}
 	}
 	QList<QPlainTextEdit*> plainTextEditList = findChildren<QPlainTextEdit*>();
 	for(int i=0;i<plainTextEditList.size();i++) {
-		ATVERIFY( connect( plainTextEditList.at(i), SIGNAL( textChanged() ), this, SLOT( slotChildWidgetModified() ) ) );
+        ATVERIFY( connect( plainTextEditList.at(i), &QPlainTextEdit::textChanged, this, &Widget::slotChildWidgetModified ) );
 	}
 	QList<QTextEdit*> textEditList = findChildren<QTextEdit*>();
 	for(int i=0;i<textEditList.size();i++) {
-		ATVERIFY( connect( textEditList.at(i), SIGNAL( textChanged() ), this, SLOT( slotChildWidgetModified() ) ) );
+        ATVERIFY( connect( textEditList.at(i), &QTextEdit::textChanged, this, &Widget::slotChildWidgetModified ) );
 	}
 	QList<QSpinBox*> spinBoxList = findChildren<QSpinBox*>();
 	for(int i=0;i<spinBoxList.size();i++) {
-		ATVERIFY( connect( spinBoxList.at(i), SIGNAL( valueChanged(int) ), this, SLOT( slotChildWidgetModified() ) ) );
+        ATVERIFY( connect( spinBoxList.at(i), &QSpinBox::valueChanged, this, &Widget::slotChildWidgetModified ) );
 	}
 	QList<QDoubleSpinBox*> doubleSpinBoxList = findChildren<QDoubleSpinBox*>();
 	for(int i=0;i<doubleSpinBoxList.size();i++) {
-		ATVERIFY( connect( doubleSpinBoxList.at(i), SIGNAL( valueChanged(double) ), this, SLOT( slotChildWidgetModified() ) ) );
+        ATVERIFY( connect( doubleSpinBoxList.at(i), &QDoubleSpinBox::valueChanged, this, &Widget::slotChildWidgetModified ) );
 	}
 	QList<QDateTimeEdit*> dateTimeEditList = findChildren<QDateTimeEdit*>();
 	for(int i=0;i<dateTimeEditList.size();i++) {
-		ATVERIFY( connect( dateTimeEditList.at(i), SIGNAL( dateTimeChanged(const QDateTime&) ), this, SLOT( slotChildWidgetModified() ) ) );
+        ATVERIFY( connect( dateTimeEditList.at(i), &QDateTimeEdit::dateTimeChanged, this, &Widget::slotChildWidgetModified ) );
 	}
 	QList<QSlider*> sliderList = findChildren<QSlider*>();
 	for(int i=0;i<sliderList.size();i++) {
-		ATVERIFY( connect( sliderList.at(i), SIGNAL( valueChanged(int) ), this, SLOT( slotChildWidgetModified() ) ) );
+        ATVERIFY( connect( sliderList.at(i), &QSlider::valueChanged, this, &Widget::slotChildWidgetModified ) );
 	}
 	QList<QDial*> dialList = findChildren<QDial*>();
 	for(int i=0;i<dialList.size();i++) {
-		ATVERIFY( connect( dialList.at(i), SIGNAL( valueChanged(int) ), this, SLOT( slotChildWidgetModified() ) ) );
+        ATVERIFY( connect( dialList.at(i), &QDial::valueChanged, this, &Widget::slotChildWidgetModified ) );
 	}
 	QList<QListWidget*> listWidgetList = findChildren<QListWidget*>();
 	for(int i=0;i<listWidgetList.size();i++) {
-		ATVERIFY( connect( listWidgetList.at(i), SIGNAL( itemChanged(QListWidgetItem*) ), this, SLOT( slotChildWidgetModified() ) ) );
+        ATVERIFY( connect( listWidgetList.at(i), &QListWidget::itemChanged, this, &Widget::slotChildWidgetModified ) );
 	}
-	QList<Widget*> myWidgetList = findChildren<Widget*>();
-	for(int i=0;i<myWidgetList.size();i++) {
-		ATVERIFY( connect( myWidgetList.at(i), SIGNAL( signalModified() ), this, SLOT( slotChildWidgetModified() ) ) );
-	}
+    // QList<Widget*> myWidgetList = findChildren<Widget*>();
+    // for(int i=0;i<myWidgetList.size();i++) {
+    //     ATVERIFY( connect( myWidgetList.at(i), &Widget::signalChildWidgetModified, this, &Widget::slotChildWidgetModified ) );
+    // }
 	QList<TableWidget*> myTableWidgetList = findChildren<TableWidget*>();
 	for(int i=0;i<myTableWidgetList.size();i++) {
-		ATVERIFY( connect( myTableWidgetList.at(i), SIGNAL( signalModified() ), this, SLOT( slotChildWidgetModified() ) ) );
+        ATVERIFY( connect( myTableWidgetList.at(i), &TableWidget::signalModified, this, &Widget::slotChildWidgetModified ) );
 	}
 	QList<TreeWidget*> myTreeWidgetList = findChildren<TreeWidget*>();
 	for(int i=0;i<myTreeWidgetList.size();i++) {
-		ATVERIFY( connect( myTreeWidgetList.at(i), SIGNAL( signalModified() ), this, SLOT( slotChildWidgetModified() ) ) );
+        ATVERIFY( connect( myTreeWidgetList.at(i), &TreeWidget::signalModified, this, &Widget::slotChildWidgetModified ) );
 	}
 }
 
